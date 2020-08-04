091e1b748e7fd61bc3f345d666044cf509588459
rizieq <rizieqgifari13012003@gmail.com>
2020-08-04 11:22:59 +0700

    delete profile

--- app/src/main/java/com/rizieq/whatsapp/activities/SignUpActivity.kt
+++ app/src/main/java/com/rizieq/whatsapp/activities/SignUpActivity.kt
@@ -113,11 +113,12 @@ class SignUpActivity : AppCompatActivity() {
                         val email = edt_email.text.toString()
                         val phone = edt_phone.text.toString()
                         val name = edt_name.text.toString()
+                        val password = edt_password.text.toString()
                         val user = User(
                             email,
                             phone,
                             name,
-                            "",
+                            password,
                             "Hello i'm new",
                             "",
                             ""
